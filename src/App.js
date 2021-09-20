ea21b1af38c23e6a03a6bc4ff6d85b38b4fd30e0
Ding <yuanli.ding@intel.com>
2021-09-19 20:52:51 -0400

    Remove redirect, not using so far

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, {Component, Fragment} from 'react';
-import {Route, Switch, Redirect} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 
 // Pages
 // import Main from './pages/Main'
