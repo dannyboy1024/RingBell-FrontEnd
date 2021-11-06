a0d41548fa3a1cb1a5bb11eaaa734496a42c2993
wm798222 <wm798222@gmail.com>
2021-11-05 22:55:57 -0500

    UserInfo push

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
